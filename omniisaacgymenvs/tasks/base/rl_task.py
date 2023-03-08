b7a47d7a7f2e62644e0348f6334ff2c5527c6ff5
ksterx <kostonerx@gmail.com>
2023-03-09 02:25:00 +0900

    add surgery env

--- omniisaacgymenvs/tasks/base/rl_task.py
+++ omniisaacgymenvs/tasks/base/rl_task.py
@@ -28,30 +28,31 @@
 
 
 from abc import abstractmethod
+
 import numpy as np
+import omni.kit
 import torch
 from gym import spaces
-from omni.isaac.core.tasks import BaseTask
-from omni.isaac.core.utils.types import ArticulationAction
-from omni.isaac.core.utils.prims import define_prim
 from omni.isaac.cloner import GridCloner
+from omni.isaac.core.tasks import BaseTask
+from omni.isaac.core.utils.prims import define_prim
+from omni.isaac.core.utils.types import ArticulationAction
+from omni.kit.viewport.utility import get_viewport_from_window_name
+from omni.kit.viewport.utility.camera_state import ViewportCameraState
 from omniisaacgymenvs.tasks.utils.usd_utils import create_distant_light
 from omniisaacgymenvs.utils.domain_randomization.randomize import Randomizer
-import omni.kit
-from omni.kit.viewport.utility.camera_state import ViewportCameraState
-from omni.kit.viewport.utility import get_viewport_from_window_name
 from pxr import Gf
+
 
 class RLTask(BaseTask):
 
-    """ This class provides a PyTorch RL-specific interface for setting up RL tasks. 
-        It includes utilities for setting up RL task related parameters,
-        cloning environments, and data collection for RL algorithms.
+    """This class provides a PyTorch RL-specific interface for setting up RL tasks.
+    It includes utilities for setting up RL task related parameters,
+    cloning environments, and data collection for RL algorithms.
     """
 
     def __init__(self, name, env, offset=None) -> None:
-
-        """ Initializes RL parameters, cloner object, and buffers.
+        """Initializes RL parameters, cloner object, and buffers.
 
         Args:
             name (str): name of the task.
@@ -86,11 +87,17 @@ class RLTask(BaseTask):
 
         # initialize data spaces (defaults to gym.Box)
         if not hasattr(self, "action_space"):
-            self.action_space = spaces.Box(np.ones(self.num_actions) * -1.0, np.ones(self.num_actions) * 1.0)
+            self.action_space = spaces.Box(
+                np.ones(self.num_actions) * -1.0, np.ones(self.num_actions) * 1.0
+            )
         if not hasattr(self, "observation_space"):
-            self.observation_space = spaces.Box(np.ones(self.num_observations) * -np.Inf, np.ones(self.num_observations) * np.Inf)
+            self.observation_space = spaces.Box(
+                np.ones(self.num_observations) * -np.Inf, np.ones(self.num_observations) * np.Inf
+            )
         if not hasattr(self, "state_space"):
-            self.state_space = spaces.Box(np.ones(self.num_states) * -np.Inf, np.ones(self.num_states) * np.Inf)
+            self.state_space = spaces.Box(
+                np.ones(self.num_states) * -np.Inf, np.ones(self.num_states) * np.Inf
+            )
 
         self._cloner = GridCloner(spacing=self._env_spacing)
         self._cloner.define_base_env(self.default_base_env_path)
@@ -99,18 +106,22 @@ class RLTask(BaseTask):
         self.cleanup()
 
     def cleanup(self) -> None:
-        """ Prepares torch buffers for RL data collection."""
+        """Prepares torch buffers for RL data collection."""
 
         # prepare tensors
-        self.obs_buf = torch.zeros((self._num_envs, self.num_observations), device=self._device, dtype=torch.float)
-        self.states_buf = torch.zeros((self._num_envs, self.num_states), device=self._device, dtype=torch.float)
+        self.obs_buf = torch.zeros(
+            (self._num_envs, self.num_observations), device=self._device, dtype=torch.float
+        )
+        self.states_buf = torch.zeros(
+            (self._num_envs, self.num_states), device=self._device, dtype=torch.float
+        )
         self.rew_buf = torch.zeros(self._num_envs, device=self._device, dtype=torch.float)
         self.reset_buf = torch.ones(self._num_envs, device=self._device, dtype=torch.long)
         self.progress_buf = torch.zeros(self._num_envs, device=self._device, dtype=torch.long)
         self.extras = {}
 
     def set_up_scene(self, scene, replicate_physics=True) -> None:
-        """ Clones environments based on value provided in task config and applies collision filters to mask 
+        """Clones environments based on value provided in task config and applies collision filters to mask
             collisions across environments.
 
         Args:
@@ -126,25 +137,39 @@ class RLTask(BaseTask):
             collision_filter_global_paths.append(self._ground_plane_path)
             scene.add_default_ground_plane(prim_path=self._ground_plane_path)
         prim_paths = self._cloner.generate_paths("/World/envs/env", self._num_envs)
-        self._env_pos = self._cloner.clone(source_prim_path="/World/envs/env_0", prim_paths=prim_paths, replicate_physics=replicate_physics)
-        self._env_pos = torch.tensor(np.array(self._env_pos), device=self._device, dtype=torch.float)
+        self._env_pos = self._cloner.clone(
+            source_prim_path="/World/envs/env_0",
+            prim_paths=prim_paths,
+            replicate_physics=replicate_physics,
+        )
+        self._env_pos = torch.tensor(
+            np.array(self._env_pos), device=self._device, dtype=torch.float
+        )
         self._cloner.filter_collisions(
-            self._env._world.get_physics_context().prim_path, "/World/collisions", prim_paths, collision_filter_global_paths)
+            self._env._world.get_physics_context().prim_path,
+            "/World/collisions",
+            prim_paths,
+            collision_filter_global_paths,
+        )
         self.set_initial_camera_params(camera_position=[10, 10, 3], camera_target=[0, 0, 0])
         if self._sim_config.task_config["sim"].get("add_distant_light", True):
             create_distant_light()
-    
+
     def set_initial_camera_params(self, camera_position=[10, 10, 3], camera_target=[0, 0, 0]):
         if self._env._render:
             viewport_api_2 = get_viewport_from_window_name("Viewport")
             viewport_api_2.set_active_camera("/OmniverseKit_Persp")
             camera_state = ViewportCameraState("/OmniverseKit_Persp", viewport_api_2)
-            camera_state.set_position_world(Gf.Vec3d(camera_position[0], camera_position[1], camera_position[2]), True)
-            camera_state.set_target_world(Gf.Vec3d(camera_target[0], camera_target[1], camera_target[2]), True)
+            camera_state.set_position_world(
+                Gf.Vec3d(camera_position[0], camera_position[1], camera_position[2]), True
+            )
+            camera_state.set_target_world(
+                Gf.Vec3d(camera_target[0], camera_target[1], camera_target[2]), True
+            )
 
     @property
     def default_base_env_path(self):
-        """ Retrieves default path to the parent of all env prims.
+        """Retrieves default path to the parent of all env prims.
 
         Returns:
             default_base_env_path(str): Defaults to "/World/envs".
@@ -153,7 +178,7 @@ class RLTask(BaseTask):
 
     @property
     def default_zero_env_path(self):
-        """ Retrieves default path to the first env prim (index 0).
+        """Retrieves default path to the first env prim (index 0).
 
         Returns:
             default_zero_env_path(str): Defaults to "/World/envs/env_0".
@@ -162,7 +187,7 @@ class RLTask(BaseTask):
 
     @property
     def num_envs(self):
-        """ Retrieves number of environments for task.
+        """Retrieves number of environments for task.
 
         Returns:
             num_envs(int): Number of environments.
@@ -171,7 +196,7 @@ class RLTask(BaseTask):
 
     @property
     def num_actions(self):
-        """ Retrieves dimension of actions.
+        """Retrieves dimension of actions.
 
         Returns:
             num_actions(int): Dimension of actions.
@@ -180,7 +205,7 @@ class RLTask(BaseTask):
 
     @property
     def num_observations(self):
-        """ Retrieves dimension of observations.
+        """Retrieves dimension of observations.
 
         Returns:
             num_observations(int): Dimension of observations.
@@ -189,7 +214,7 @@ class RLTask(BaseTask):
 
     @property
     def num_states(self):
-        """ Retrieves dimesion of states.
+        """Retrieves dimesion of states.
 
         Returns:
             num_states(int): Dimension of states.
@@ -198,7 +223,7 @@ class RLTask(BaseTask):
 
     @property
     def num_agents(self):
-        """ Retrieves number of agents for multi-agent environments.
+        """Retrieves number of agents for multi-agent environments.
 
         Returns:
             num_agents(int): Dimension of states.
@@ -206,7 +231,7 @@ class RLTask(BaseTask):
         return self._num_agents
 
     def get_states(self):
-        """ API for retrieving states buffer, used for asymmetric AC training.
+        """API for retrieving states buffer, used for asymmetric AC training.
 
         Returns:
             states_buf(torch.Tensor): States buffer.
@@ -214,7 +239,7 @@ class RLTask(BaseTask):
         return self.states_buf
 
     def get_extras(self):
-        """ API for retrieving extras data for RL.
+        """API for retrieving extras data for RL.
 
         Returns:
             extras(dict): Dictionary containing extras data.
@@ -222,12 +247,11 @@ class RLTask(BaseTask):
         return self.extras
 
     def reset(self):
-        """ Flags all environments for reset.
-        """
+        """Flags all environments for reset."""
         self.reset_buf = torch.ones_like(self.reset_buf)
 
     def pre_physics_step(self, actions):
-        """ Optionally implemented by individual task classes to process actions.
+        """Optionally implemented by individual task classes to process actions.
 
         Args:
             actions (torch.Tensor): Actions generated by RL policy.
@@ -235,7 +259,7 @@ class RLTask(BaseTask):
         pass
 
     def post_physics_step(self):
-        """ Processes RL required computations for observations, states, rewards, resets, and extras.
+        """Processes RL required computations for observations, states, rewards, resets, and extras.
             Also maintains progress buffer for tracking step count per environment.
 
         Returns:
